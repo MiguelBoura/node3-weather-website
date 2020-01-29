5d2e3cad3ff29edbe264b98b7a84600d4f21f849
MiguelBoura <miguel@iotiq.com.au>
2020-01-30 00:03:06 +1000

    Add forecast data text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Please try again', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. If this percent is zero, no need for an umbrella, mate. The wind speed is ' + body.currently.windSpeed + ' something...')
         }
     })
 }
